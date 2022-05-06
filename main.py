1e79675173e6ac11b7677445525bb1e13aea2597
Anton <kondratov247@mail.ru>
2022-05-06 15:10:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)  # Подключение к web-камере 
 mp_Hands = mp.solutions.hands  # говорим, что хотим распознать руки
@@ -7,13 +8,14 @@ hands = mp_Hands.Hands(max_num_hands = 2)  # характеристики для
 mpDraw = mp.solutions.drawing_utils  # инициализация утилит рисования
 
 fingers_coord = [(8, 6), (12, 10), (16, 14), (20, 18)]  # координаты ключевых точек на руке, кроме большого пальца
-thumb_coord = (4, 2) # координаты ключевых точек для большого пальца
+thumb_coord = (4, 3) # координаты ключевых точек для большого пальца
 
 while cap.isOpened():  # пока камера "работает"
     success, image = cap.read()  # получаем кадр с камеры
     if not success: # если не удалось получить кадр с камеры
         print('Не удалось получить кадр с web-камеры')
         continue  # переход к ближайшему циклу (while 12 строчка)
+    prevTime = time.time()
     image = cv2.flip(image, 1)   # зеркально отражаем изображение
     RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  #BGR -> RGB изображение
     results = hands.process(RGB_image)  # ищем руки на изображении
@@ -27,7 +29,6 @@ while cap.isOpened():  # пока камера "работает"
 
             mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)
             fingersList = [] 
-            #ВНИМАНИЕ!!!
             for lm in handLms.landmark: # перебираем ключевые точки на руке
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
@@ -35,9 +36,23 @@ while cap.isOpened():  # пока камера "работает"
             for coordinate in fingers_coord:
                 if fingersList[coordinate[0]][1] < fingersList[coordinate[1]][1]:
                     upCount += 1
+            
+            side = 'left'
+            if fingersList[5][0] > fingersList[17][0]:
+                side = 'right'
+            
+            if side == 'left':
+                if fingersList[thumb_coord[0]][0] < fingersList[thumb_coord[1]][0]:
+                    upCount += 1
+            else:
+                if fingersList[thumb_coord[0]][0] > fingersList[thumb_coord[1]][0]:
+                    upCount += 1
+        cv2.putText(image, str(upCount), (50, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (50, 200, 255), 5)
         print(upCount)
 
-
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    cv2.putText(image, f"FPS: {fps}", (150, 100), cv2.FONT_HERSHEY_DUPLEX, 3, (200, 200, 200), 3 )
     cv2.imshow('web-cam', image)
     if cv2.waitKey(1) & 0xFF == 27: # ожидаем нажание клавиши ESC
         break
